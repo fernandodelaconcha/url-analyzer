7bc1bd44d3a2cb94e07b244b6ab45e4c54cc38c0
Fernando <fernandodelaconcha@gmail.com>
2017-07-27 14:14:56 +0200

    server updated with a more detailed response

--- app/server.js
+++ app/server.js
@@ -1,17 +1,36 @@
+// require imports
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM(`<!DOCTYPE html>`);
+//const $ = require('jQuery')(window);
 var express = require('express');
 var app = express();
 var path = require('path');
 var http = require ('http');
 var request = require('request');
 var cheerio = require('cheerio');
-
+//loads the view using express
 app.use(express.static(__dirname + '/../public'));
-
+//takes the search term as a parameter in order to make the request
 app.get('/api', function(req, res) {
     var searchTerm = req.query;
+    var result = "";
 	request(searchTerm.url, function (error, response, html) {
+		// gets the desired html source code with cheerio and filters the requested items creating a JSON
   		if (!error && response.statusCode == 200) {
-    		res.send(html);
+  			var $ = cheerio.load(html);
+	        var webpageTitle = $("title").text();
+	        var links = $("body").find('a').length;
+	        var webpage = {
+	        	htmVersion: "",
+        		title: webpageTitle,
+        		headings: "",
+        		links: links,
+        		hiddenLinks: "",
+        		loginForm: ""
+        	}
+        	//sends the JSON with the filtered information as a response
+        	res.send(webpage);
 		}
 	});
 });
--- public/app.js
+++ public/app.js
@@ -1,5 +1,8 @@
-$(document).ready(function () {
-  //your code here
-
-  console.log('hi');
-});
+// $(document).ready(function () {
+// 	$('form').submit(function(event) {
+//     	event.preventDefault();
+// 		$.get('/api', function(data) {
+//     		$('.result').html(data);
+//     	});
+// 	});
+// });
