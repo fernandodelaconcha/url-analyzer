edf32797d12614d8a828a1245a5af5023df7cf36
Fernando <fernandodelaconcha@gmail.com>
2017-07-28 10:50:17 +0200

    app already working

--- app/server.js
+++ app/server.js
@@ -19,18 +19,59 @@ app.get('/api', function(req, res) {
 		// gets the desired html source code with cheerio and filters the requested items creating a JSON
   		if (!error && response.statusCode == 200) {
   			var $ = cheerio.load(html);
+  			var htmlVersion; 
+  			if (String(html).indexOf('<!doctype html>') == -1){
+  				htmlVersion='HTML 4.01 or older';
+  				} else {
+  					htmlVersion='HTML 5';
+  				}
 	        var webpageTitle = $("title").text();
-	        var links = $("body").find('a').length;
+	        var h1 = $("body").find('h1');
+	        var h2 = $("body").find('h2');
+	        var h3 = $("body").find('h3');
+	        var h4 = $("body").find('h4');
+	        var h5 = $("body").find('h5');
+	        var h6 = $("body").find('h6');
+
+	        var links = $("body").find('a');
+	        var internalLinks= [];
+	        var emptyLinks = [];
+			links.each(function(i, link) {
+            if($(link).attr("href").indexOf('/') == 0){
+	            internalLinks[i]=link; 
+	            };
+	        });
+            links.each(function(i, link) {
+            if($(link).attr("href") == ""){
+            	emptyLinks[i]=link; 
+	            };
+	        });
+
+	        var loginForm;
+	        
+	        if ($('form').attr('method') == "post"){
+	        	loginForm = 'Yes';
+	        } else {
+	        	loginForm = 'No';
+	        }
+
 	        var webpage = {
-	        	htmVersion: "",
+	        	htmlVersion: htmlVersion,
         		title: webpageTitle,
-        		headings: "",
-        		links: links,
-        		hiddenLinks: "",
-        		loginForm: ""
+        		headings: h1.length + h2.length + h3.length + h4.length + h5.length + h6.length,
+        		h1: h1.length,
+        		h2: h2.length,
+        		h3: h3.length,
+        		h4: h4.length,
+        		h5: h5.length,
+        		h6: h6.length,
+        		links: links.length,
+        		internalLinks: internalLinks.length,
+        		emptyLinks: emptyLinks.length,
+        		loginForm: loginForm
         	}
         	//sends the JSON with the filtered information as a response
-        	res.send(webpage);
+        	res.json(webpage);
 		}
 	});
 });
--- public/app.js
+++ public/app.js
@@ -1,8 +1,21 @@
-// $(document).ready(function () {
-// 	$('form').submit(function(event) {
-//     	event.preventDefault();
-// 		$.get('/api', function(data) {
-//     		$('.result').html(data);
-//     	});
-// 	});
-// });
+$(document).ready(function(){
+   var $form = $('form');
+   $form.submit(function(){
+      $.get($(this).attr('action'), $(this).serialize(), function(data){
+            $('#htmlVersion').empty().append('<p>'+data.htmlVersion+'</p>');
+            $('#title').empty().append('<p>'+data.title+'</p>');
+            $('#headings').empty().append('<p>'+data.headings+'</p>');
+            $('#h1').empty().append('<p>'+data.h1+'</p>');
+            $('#h2').empty().append('<p>'+data.h2+'</p>');
+            $('#h3').empty().append('<p>'+data.h3+'</p>');
+            $('#h4').empty().append('<p>'+data.h4+'</p>');
+            $('#h5').empty().append('<p>'+data.h5+'</p>');
+            $('#h6').empty().append('<p>'+data.h6+'</p>');
+            $('#links').empty().append('<p>'+data.links+'</p>');
+            $('#internalLinks').empty().append('<p>'+data.internalLinks+'</p>');
+            $('#emptyLinks').empty().append('<p>'+data.emptyLinks+'</p>');
+            $('#loginForm').empty().append('<p>'+data.loginForm+'</p>');
+      },'json');
+      return false;
+   });
+});
